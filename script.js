a1dacdf3c0da3ccf44718fec3d57fa955e6636a3
gabrielvcb7 <gabriel.00000849430@unicap.br>
2024-11-08 19:52:10 -0300

    Question2

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 // Altera o texto do parágrafo com id "texto"
 document.getElementById("texto").innerText = "Texto atualizado via DOM";
 
+// Exibe a mesma mensagem no console do navegador
+console.log("Bem-vindo(a) ao nosso site!");
+
 // Função para exibir uma mensagem de boas-vindas
 function exibirMensagem() {
     // Exibe a mensagem em uma janela de alerta
     window.alert("Bem-vindo(a) ao nosso site!");
-    // Exibe a mesma mensagem no console do navegador
-    console.log("Bem-vindo(a) ao nosso site!");
 }
